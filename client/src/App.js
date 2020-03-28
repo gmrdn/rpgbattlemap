18b7422cd2aa8397fa353f45233e5d0cf182dfc6
Guillaume R <guillaume.rondon@gmail.com>
2020-03-28 20:53:39 +0100

    ♻️ Cleanup and folders reorganization

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Grid from "./components/Grid";
+
+const App = () => {
+  return (
+    <div>
+      <Grid></Grid>
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/components/Grid.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Grid = () => <div></div>;
+
+export default Grid;
