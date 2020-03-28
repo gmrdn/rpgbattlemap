aa8bb9b3f23bb69d9da9de90570658d3c8bcccc2
Guillaume R <guillaume.rondon@gmail.com>
2020-03-28 21:50:19 +0100

    Added server.js and components skeleton

---
+++ client/src/components/Chatbox.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Chatbox = () => <div></div>;
+
+export default Chatbox;
---
+++ client/src/components/Room.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Room = () => <div></div>;
+
+export default Room;
