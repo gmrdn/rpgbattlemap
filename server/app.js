8f809660c04b664f11e67df862f69ae59eb16550
Guillaume R <guillaume.rondon@gmail.com>
2020-04-03 22:46:06 +0200

    Testing with Jest and Babel 7 setup

---
+++ server/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import routes from "./src/routes/roomRoutes";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+
+const app = express();
+
+// mongoose connection
+const connectionString =
+  "mongodb+srv://admin:" +
+  process.env.MONGOPWD +
+  "@dndbattlemap-1236i.mongodb.net/test?retryWrites=true&w=majority";
+
+mongoose.Promise = global.Promise;
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// body parser setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+routes(app);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send(`Whoops ! ${err.stack}`);
+});
+
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,39 +1,5 @@
-import express from "express";
-import routes from "./src/routes/roomRoutes";
-import mongoose from "mongoose";
-import bodyParser from "body-parser";
-import data from "./mocks/rooms.json";
-
-const app = express();
+const app = require("./app");
 const PORT = 5000;
-
-// mongoose connection
-const connectionString =
-  "mongodb+srv://admin:" +
-  process.env.MONGOPWD +
-  "@dndbattlemap-1236i.mongodb.net/test?retryWrites=true&w=majority";
-
-mongoose.Promise = global.Promise;
-mongoose.connect(connectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
-// body parser setup
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-routes(app);
-
-app.get("/api/grid/:id", (req, res) => {
-  const filtered = data.rooms.filter(room => room._id == req.params.id);
-  res.json(filtered[0].grid);
-});
-
-// app.use((err, req, res, next) => {
-//   console.error(err.stack);
-//   res.status(500).send(`Whoops ! ${err.stack}`);
-// });
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
