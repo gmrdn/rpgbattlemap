e9068a2835a3d1d91cb1ea830b088c178a444b42
Guillaume R <guillaume.rondon@gmail.com>
2020-04-05 21:13:12 +0200

    Added dotenv config file

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ import express from "express";
 import routes from "./src/routes/roomRoutes";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
+import path from "path";
+
+require("dotenv").config();
 
 const app = express();
 
@@ -20,12 +23,17 @@ mongoose.connect(connectionString, {
 // body parser setup
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 routes(app);
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send(`Whoops ! ${err.stack}`);
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require("./app");
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
