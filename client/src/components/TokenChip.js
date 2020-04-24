e270187ab70da2d5aac7d8cc1f56074b17480dda
Guillaume R <guillaume.rondon@gmail.com>
2020-04-24 22:39:16 +0200

    Connected TokenChips to redux store (clicking selects the token on the grid)

--- client/src/components/TokenChip.js
+++ client/src/components/TokenChip.js
@@ -1,34 +1,58 @@
 import React from "react";
 import Avatar from "@material-ui/core/Avatar";
 import Chip from "@material-ui/core/Chip";
+import { connect } from "react-redux";
+import { selectToken } from "../actions";
 
-const TokenChip = (props) => {
-  const handleDelete = () => {
+class TokenChip extends React.Component {
+  handleDelete = () => {
     console.info("You clicked the delete icon.");
   };
-  const handleClick = () => {
+
+  handleClick = (e) => {
+    e.preventDefault();
+    const tokenId = e.target.parentNode.id;
     console.info("You clicked the Chip.");
+    console.log(tokenId);
+    this.props.selectToken(tokenId);
   };
-  return (
-    <div id={`tokenchip-${props.token.x}-${props.token.y}`}>
-      <Chip
-        size="small"
-        avatar={
-          <Avatar
-            alt={props.token.name}
-            src={`/tokens/${props.token.image}`}
-            style={{
-              backgroundColor: props.token.color,
-            }}
-          />
-        }
-        label={props.token.name}
-        color={props.token.selected ? "primary" : ""}
-        onClick={handleClick}
-        onDelete={handleDelete}
-      />
-    </div>
-  );
+
+  render() {
+    return (
+      <div id={`tokenchip-${this.props.token.x}-${this.props.token.y}`}>
+        <Chip
+          size="small"
+          avatar={
+            <Avatar
+              alt={this.props.token.name}
+              src={`/tokens/${this.props.token.image}`}
+              style={{
+                backgroundColor: this.props.token.color,
+              }}
+            />
+          }
+          label={this.props.token.name}
+          color={this.props.token.selected ? "primary" : ""}
+          id={this.props.token._id}
+          onClick={this.handleClick}
+          onDelete={this.handleDelete}
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    nickname: state.nickname,
+    roomId: state.roomId,
+    tokens: state.tokens,
+  };
 };
 
-export default TokenChip;
+const mapDispatchToProps = {
+  selectToken,
+};
+
+// export default TokenChip;
+export default connect(mapStateToProps, mapDispatchToProps)(TokenChip);
