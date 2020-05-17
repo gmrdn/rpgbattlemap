1d2b5755c3d249de154f21c77e1f31a7e06145ac
Guillaume R <guillaume.rondon@gmail.com>
2020-05-17 21:45:28 +0200

    ✅ Canvas unit tests

--- client/src/__tests__/components/Grid.spec.js
+++ client/src/__tests__/components/Grid.spec.js
@@ -8,6 +8,7 @@ import axios from "axios";
 import * as data from "../mocks/room-with-tokens.json";
 
 let wrapper;
+let canvas;
 const mockTokens = [
   {
     _id: "1111111111",
@@ -57,6 +58,7 @@ describe("Components", () => {
         .mockImplementation(() => Promise.resolve(mockData));
 
       jest.spyOn(canvasUtils, "drawGrid").mockImplementation(() => false);
+      jest.clearAllMocks();
     });
 
     describe("General display", () => {
@@ -112,80 +114,165 @@ describe("Components", () => {
     });
 
     describe("User events", () => {
-      it("selects a token on left click", async () => {
-        //when the avatar has an image
-        const mockEventWithId = {
-          preventDefault: () => {},
-          target: {
-            id: "1234567890",
-            parentNode: {
-              id: "",
-            },
-          },
-        };
-        //when avatar has no image, get the id from parent node
-        const mockEventWithoutId = {
-          preventDefault: () => {},
-          target: {
-            id: "",
-            parentNode: {
+      describe("Actions on Token", () => {
+        it("selects a token on left click", async () => {
+          //when the avatar has an image
+          const mockEventWithId = {
+            preventDefault: () => {},
+            target: {
               id: "1234567890",
+              parentNode: {
+                id: "",
+              },
             },
-          },
-        };
+          };
+          //when avatar has no image, get the id from parent node
+          const mockEventWithoutId = {
+            preventDefault: () => {},
+            target: {
+              id: "",
+              parentNode: {
+                id: "1234567890",
+              },
+            },
+          };
 
-        const token = {
-          _id: "1234567890",
-          x: 1,
-          y: 1,
-          image: "1.png",
-          color: "red",
-          name: "Jest Token",
-          selected: false,
-        };
+          const token = {
+            _id: "1234567890",
+            x: 1,
+            y: 1,
+            image: "1.png",
+            color: "red",
+            name: "Jest Token",
+            selected: false,
+          };
 
-        wrapper = await mount(
-          <Grid
-            roomId={roomId}
-            nickname={nickname}
-            socket={socket}
-            resetTokens={resetTokensMock}
-            selectToken={selectTokenMock}
-            tokens={[token]}
-          />
-        );
+          wrapper = await mount(
+            <Grid
+              roomId={roomId}
+              nickname={nickname}
+              socket={socket}
+              resetTokens={resetTokensMock}
+              selectToken={selectTokenMock}
+              tokens={[token]}
+            />
+          );
 
-        const tokenElement = wrapper.find(Token);
+          const tokenElement = wrapper.find(Token);
 
-        tokenElement.simulate("click", mockEventWithId);
-        expect(selectTokenMock).toHaveBeenCalledWith("1234567890");
+          tokenElement.simulate("click", mockEventWithId);
+          expect(selectTokenMock).toHaveBeenCalledWith("1234567890");
 
-        tokenElement.simulate("click", mockEventWithoutId);
-        expect(selectTokenMock).toHaveBeenCalledWith("1234567890");
+          tokenElement.simulate("click", mockEventWithoutId);
+          expect(selectTokenMock).toHaveBeenCalledWith("1234567890");
+        });
       });
+      describe("Actions on the canvas", () => {
+        beforeEach(async () => {
+          wrapper = await mount(
+            <Grid
+              roomId={roomId}
+              nickname={nickname}
+              socket={socket}
+              resetTokens={resetTokensMock}
+              selectToken={selectTokenMock}
+              moveToken={moveTokenMock}
+              tokens={mockTokens}
+            />
+          );
 
-      it("prevents right click context menu on the canvas", async () => {
-        const mockEvent = {
-          preventDefault: jest.fn(),
-        };
+          canvas = wrapper.find("canvas");
+        });
+        it("prevents right click context menu on the canvas", async () => {
+          const mockEvent = {
+            preventDefault: jest.fn(),
+          };
 
-        const spy = jest.spyOn(mockEvent, "preventDefault");
+          const spy = jest.spyOn(mockEvent, "preventDefault");
+          act(() => {
+            canvas.simulate("contextmenu", mockEvent);
+          });
+          expect(spy).toHaveBeenCalled();
+        });
 
-        wrapper = await mount(
-          <Grid
-            roomId={roomId}
-            nickname={nickname}
-            socket={socket}
-            resetTokens={resetTokensMock}
-            selectToken={selectTokenMock}
-            tokens={mockTokens}
-          />
-        );
+        it("changes token coordinates on mouse down", async () => {
+          act(() => {
+            canvas.prop("onMouseDown")({
+              clientX: 160,
+              clientY: 240,
+              button: 0,
+            });
+          });
+          expect(wrapper.prop("moveToken")).toHaveBeenCalled();
+          expect(socket.emit).toHaveBeenCalled();
+          expect(socket.emit.mock.calls[0]).toMatchObject([
+            "moveToken",
+            { room: roomId, user: nickname, tokenId: "1111111111", x: 4, y: 6 },
+          ]);
+        });
 
-        const canvas = wrapper.find("canvas");
+        it("doesn't change coordinates when no token is selected", async () => {
+          const mockTokens = [
+            {
+              _id: "1111111111",
+              x: 1,
+              y: 1,
+              name: "Mock Token not selected and to be deleted",
+              toBeDeleted: true,
+              selected: false,
+            },
+            {
+              _id: "2222222222",
+              x: 1,
+              y: 1,
+              name: "Mock Token not selected but to be deleted",
+              toBeDeleted: true,
+              selected: false,
+            },
+            {
+              _id: "3333333333",
+              x: 1,
+              y: 1,
+              name: "Mock Token not selected and not to be deleted",
+              toBeDeleted: false,
+              selected: false,
+            },
+          ];
+          wrapper = await mount(
+            <Grid
+              roomId={roomId}
+              nickname={nickname}
+              socket={socket}
+              resetTokens={resetTokensMock}
+              selectToken={selectTokenMock}
+              moveToken={moveTokenMock}
+              tokens={mockTokens}
+            />
+          );
 
-        canvas.simulate("contextmenu", mockEvent);
-        expect(spy).toHaveBeenCalled();
+          canvas = await wrapper.find("canvas");
+
+          act(() => {
+            canvas.prop("onMouseDown")({
+              clientX: 160,
+              clientY: 240,
+              button: 0,
+            });
+          });
+          expect(wrapper.prop("moveToken")).not.toHaveBeenCalled();
+          expect(socket.emit).not.toHaveBeenCalled();
+        });
+        it("doesn't change coordinates when the click is not the left button", async () => {
+          act(() => {
+            canvas.prop("onMouseDown")({
+              clientX: 160,
+              clientY: 240,
+              button: 1,
+            });
+          });
+          expect(wrapper.prop("moveToken")).not.toHaveBeenCalled();
+          expect(socket.emit).not.toHaveBeenCalled();
+        });
       });
     });
   });
