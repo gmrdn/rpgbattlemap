584fbdf011e7971961740b4cb7d2ae546c1ca1dd
Guillaume R <guillaume.rondon@gmail.com>
2020-05-17 23:56:18 +0200

    ♻️ Grid drawing refactoring

--- client/src/utils/canvasUtils.js
+++ client/src/utils/canvasUtils.js
@@ -1,12 +1,24 @@
-const drawGrid = (ctx, gridSize, tileSide) => {
-  for (var y = 0; y < gridSize.height; y++) {
-    for (var x = 0; x < gridSize.width; x++) {
-      ctx.lineWidth = 0.2;
-      ctx.beginPath();
-      ctx.rect(x * tileSide, y * tileSide, tileSide, tileSide);
-      ctx.stroke();
-    }
+const LINE_WIDTH = 0.2;
+
+const drawGrid = (canvas, gridSize, tileSide) => {
+  for (let currentRow = 0; currentRow < gridSize.height; currentRow++) {
+    drawGridRow(gridSize, canvas, tileSide, currentRow);
   }
+};
+
+const drawGridRow = (gridSize, canvas, tileSide, currentRow) => {
+  canvas.lineWidth = LINE_WIDTH;
+  for (let currentCol = 0; currentCol < gridSize.width; currentCol++) {
+    canvas.beginPath();
+    drawCell(canvas, currentCol, tileSide, currentRow);
+    canvas.stroke();
+  }
+};
+
+const drawCell = (canvas, currentCol, tileSide, currentRow) => {
+  const startingPositionLeft = currentCol * tileSide;
+  const startingPositionTop = currentRow * tileSide;
+  canvas.rect(startingPositionLeft, startingPositionTop, tileSide, tileSide);
 };
 
 const canvasUtils = {
