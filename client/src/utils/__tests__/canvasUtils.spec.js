9ddacd9c9a484428c07b9ba06c921907f902a84d
Guillaume R <guillaume.rondon@gmail.com>
2020-06-02 22:16:04 +0200

    ⚡️ Reduced canvas size in the tests

--- client/src/utils/__tests__/canvasUtils.spec.js
+++ client/src/utils/__tests__/canvasUtils.spec.js
@@ -9,10 +9,10 @@ describe("Utils", () => {
     it("draws a grid", () => {
       const canvas = document.createElement("canvas");
       canvas.id = "canvas";
-      canvas.setAttribute("width", "2400");
-      canvas.setAttribute("height", "2400");
+      canvas.setAttribute("width", "160");
+      canvas.setAttribute("height", "160");
       const ctx = canvas.getContext("2d");
-      const gridSize = { width: 60, height: 60 };
+      const gridSize = { width: 4, height: 4 };
       const tileSide = 40;
       canvasUtils.drawGrid(ctx, gridSize, tileSide);
 
