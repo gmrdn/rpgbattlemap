3c0af050bcd34385bd5e5bfe9849642fcde18dce
Guillaume R <guillaume.rondon@gmail.com>
2020-07-08 16:57:11 +0200

    Added Alt text for Img

--- client/src/components/Logo.js
+++ client/src/components/Logo.js
@@ -4,7 +4,11 @@ import { Box } from "@material-ui/core";
 const Logo = () => {
   return (
     <Box mt={5} mb={5}>
-      <img src="undraw_hang_out_h9ud.svg" height="287px"></img>
+      <img
+        src="undraw_hang_out_h9ud.svg"
+        alt="Friends sitting at a table"
+        height="287px"
+      ></img>
     </Box>
   );
 };
